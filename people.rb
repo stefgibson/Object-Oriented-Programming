8a6a52e49a93f1326f3fa15f9df072827ce543e9
Stefanie Gibson <gibsonstefanie@gmail.com>
2014-08-14 15:15:00 -0400

    Completed exercise

--- people.rb
+++ people.rb
@@ -1,33 +1,59 @@
-class Students
-  def students_say
-   "I get it"
- end
-  def initialize
-   "I get it"
- end
-def to_s
-  "I get it"
- end
+class Person
+  attr_reader  :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    name
+  end
+
+  def greeting
+    puts "Hi, my name is #{@name}"
+  end
 end
 
-class Instructor
-  def instr_say
-   "Everything in Ruby is an Object"
- end
-  def initialize
-   "Everything in Ruby is an Object"
-end
-def to_s
-  "Everything in Ruby is an Object"
- end
+class Student < Person
+  def learn
+    puts "I get it"
+  end
 
+  def to_s
+    "I get it"
+  end
 end
 
-a = Students.new
-a.students_say
-b = Instructor.new
-b.instr_say
+class Instructor < Person
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
 
-puts a
+  def to_s
+    "Everything in Ruby is an Object"
+  end
+end
 
-puts b
+
+s = Student.new("Chris")
+s.greeting
+
+i = Instructor.new("Christina")
+i.greeting
+
+# In order for the teach method to work on student, the Instructor class would need to be a parent to Student. As it stands, the Student class will not inherit any of the properties from the Instructor class.
+
+
+# name = Person.new("Stef")
+# puts name
+
+# a = Student.new
+# a.learn
+# b = Instructor.new
+# b.teach
+
+# puts a
+
+# puts b
+
+
